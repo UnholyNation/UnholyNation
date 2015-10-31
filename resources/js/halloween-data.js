8752c4cd229caf813fbad49bc886fea221ca0477
Jave L. Cross <javecross.graal@gmail.com>
2015-10-31 11:52:03 -0500

    Adding new Time results.

--- resources/js/halloween-data.js
+++ resources/js/halloween-data.js
@@ -9,14 +9,18 @@ com.javecross.HalloweenParser = (function() {
 		halloweenParticipants,
 		totalRuns,
 		totalCandy,
-		myCurrentChart;
+		myCurrentChart,
+		totalTime,
+		averageTime,
+		globalFastest;
 
 	function parseRow(rowText) {
 		var rowData = {},
 			tokens = rowText.split('.'),
 			acc,
 			param,
-			value;
+			value,
+			time;
 		if (tokens.length < 4) {
 			throw new Error('Unable to parse, incorrect token count (' + tokens.length + ')!');
 		}
@@ -37,6 +41,7 @@ com.javecross.HalloweenParser = (function() {
 			halloweenResults[acc] = {};
 			halloweenResults[acc].runs = [];
 			halloweenParticipants += 1;
+			halloweenResults[acc].totalTime = 0;
 		}
 		if (param === 'runs') {
 			totalRuns += parseInt(value);
@@ -48,7 +53,30 @@ com.javecross.HalloweenParser = (function() {
 		}
 		if (parseInt(param) >= 0) {
 			// It's a run time.
-			halloweenResults[acc].runs.push(value);			
+			time = parseFloat(value);
+			halloweenResults[acc].runs.push(time);	
+			totalTime += time;
+			halloweenResults[acc].totalTime += time;
+			if (!halloweenResults[acc].fastestTime) {
+				halloweenResults[acc].fastestTime = time;
+			} else {
+				if (time < halloweenResults[acc].fastestTime) {
+					halloweenResults[acc].fastestTime = time;
+				}
+			}
+			if (!globalFastest) {
+				globalFastest = {
+					'account': acc,
+					'time': time
+				};
+			} else {
+				if (time < globalFastest.time) {
+					globalFastest = {
+						'account': acc,
+						'time': time
+					};
+				}
+			}
 		}				
 	}
 	function createNewCanvasElement() {
@@ -79,6 +107,9 @@ com.javecross.HalloweenParser = (function() {
 		$('#local-runs').text(user.totalRuns);
 		$('#local-candy').text(user.candy);
 		$('#local-title').text(value + ' Overview');
+		$('#local-avg-time').text(user.totalTime/user.totalRuns);
+		$('#local-total-time').text(user.totalTime);
+		$('#fast-time').text(user.fastestTime);
 		$('section .local-info').removeClass('hidden');
 		if ($('#halloween-canvas').length) {
 			$('#halloween-canvas').remove();
@@ -107,6 +138,15 @@ com.javecross.HalloweenParser = (function() {
 		createNewCanvasElement();
 		var ctx = document.getElementById("halloween-canvas").getContext("2d");
 		myCurrentChart = new Chart(ctx).Bar(getGlobalCandyDataSet());
+	}
+	function viewGlobalTimes() {
+		$('#local-title').html('Global Time Overview (Average Time/Fastest Time)<br>Larger numbers are faster times');
+		if ($('#halloween-canvas').length) {
+			$('#halloween-canvas').remove();
+		}
+		createNewCanvasElement();
+		var ctx = document.getElementById("halloween-canvas").getContext("2d");
+		myCurrentChart = new Chart(ctx).Bar(getGlobalTimeDataSet());
 	}
 	
 	function getUserDataSet(userInfo) {
@@ -157,6 +197,29 @@ com.javecross.HalloweenParser = (function() {
 		data.datasets.push(dataSet);
 		return data;
 	}
+	function getGlobalTimeDataSet() {
+		var data = {
+				'labels': [],
+				'datasets': []
+			},
+			dataSet = {
+				'label': "Global Time Dataset",
+				'fillColor': "#E64A19",
+				'strokeColor': "#E64A19",
+				'highlightFill': "#FFEB3B",
+				'highlightStroke': "#FFEB3B",
+				'data': []
+			};
+		
+		$.each(halloweenResults, function(key, elem) {
+			if (elem.fastestTime && elem.fastestTime <= 700) {
+				data.labels.push(key);
+				dataSet.data.push(averageTime/elem.fastestTime);
+			}
+		});
+		data.datasets.push(dataSet);
+		return data;
+	}
 	function getGlobalCandyDataSet() {
 		var data = {
 				'labels': [],
@@ -193,6 +256,7 @@ com.javecross.HalloweenParser = (function() {
 			halloweenParticipants = 0;
 			totalRuns = 0;
 			totalCandy = 0;
+			totalTime = 0;
 			
 			for (i = 0, size = rows.length; i < size; i++) {
 				row = rows[i];
@@ -205,6 +269,7 @@ com.javecross.HalloweenParser = (function() {
 					//console.log('Row is not parseable: ' + row + ', ' + Error);
 				}
 			}
+			averageTime = totalTime / totalRuns;
 		},
 		buildResultDisplay: function () {
 			createNewCanvasElement();
@@ -214,6 +279,9 @@ com.javecross.HalloweenParser = (function() {
 			$('#total-parts').text(halloweenParticipants);
 			$('#total-runs').text(totalRuns);
 			$('#total-candy').text(totalCandy);
+			$('#avg-time').text(averageTime/60 + ' minutes');
+			$('#total-time').text(totalTime/60/60 + ' hours');
+			$('#global-fastest').text(globalFastest.account + ': ' + globalFastest.time + ' seconds');
 			$.each(halloweenResults, function (key, _) {
 				$('#account-selector').append($('<option>', { 
 					value: key,
@@ -227,6 +295,9 @@ com.javecross.HalloweenParser = (function() {
 			});
 			$('#see-runs').on('click', function() {				
 				viewGlobalRuns();
+			});
+			$('#see-times').on('click', function() {
+				viewGlobalTimes();
 			});
 			viewGlobalCandy();			
 		},
